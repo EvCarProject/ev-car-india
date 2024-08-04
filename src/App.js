40356709dc317865d6138d7aa881b550ba85df04
evcarindia <ganeshi82@gmail.com>
2024-08-04 17:23:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainHeader from './MainHeader';
+
+function App() {
+  return (
+      <div className="App">
+          <MainHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './topHeader.css'
+
+const MainHeader = () => {
+    return (
+        <div>
+        <div>
+            <img src='./img/EVLogo.jfif'></img>
+        </div>
+        </div>
+    )
+}
+
+export default MainHeader
